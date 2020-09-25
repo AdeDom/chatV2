41359247596d984ac1859168b200e9d94f0cf150
AdeDom <pathiphonyw@gmail.com>
2020-09-25 15:38:29 +0700

    dru-chat refactor 3

--- src/Application.kt
+++ src/Application.kt
@@ -13,7 +13,7 @@ import io.ktor.routing.*
 import io.ktor.sessions.*
 import io.ktor.util.*
 import io.ktor.websocket.*
-import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.receiveAsFlow
@@ -22,6 +22,10 @@ import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Duration
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.atomic.AtomicInteger
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -142,12 +146,8 @@ fun Application.module() {
 
                 server.memberJoin(session.id, this)
 
-                try {
-                    incoming.receiveAsFlow().collect { frame ->
-                        receivedMessage(session.id, (frame as Frame.Text).readText())
-                    }
-                } finally {
-                    server.memberLeft(session.id, this)
+                incoming.receiveAsFlow().collect { frame ->
+                    receivedMessage(session.id, (frame as Frame.Text).readText())
                 }
             }
         }
@@ -183,3 +183,95 @@ private suspend fun receivedMessage(id: String, command: String) {
 }
 
 private val server = ChatServer()
+
+class ChatServer {
+
+    val usersCounter = AtomicInteger()
+
+    val memberNames = ConcurrentHashMap<String, String>()
+
+    val members = ConcurrentHashMap<String, MutableList<WebSocketSession>>()
+
+    val lastMessages = LinkedList<String>()
+
+    suspend fun memberJoin(member: String, socket: WebSocketSession) {
+        val name = memberNames.computeIfAbsent(member) { "user${usersCounter.incrementAndGet()}" }
+
+        val list = members.computeIfAbsent(member) { CopyOnWriteArrayList<WebSocketSession>() }
+        list.add(socket)
+
+        if (list.size == 1) {
+            broadcast("server", "Member joined: $name.")
+        }
+
+        val messages = synchronized(lastMessages) { lastMessages.toList() }
+        for (message in messages) {
+            socket.send(Frame.Text(message))
+        }
+    }
+
+    suspend fun memberRenamed(member: String, to: String) {
+        val oldName = memberNames.put(member, to) ?: member
+        broadcast("server", "Member renamed from $oldName to $to")
+    }
+
+    suspend fun memberLeft(member: String, socket: WebSocketSession) {
+        val connections = members[member]
+        connections?.remove(socket)
+
+        if (connections != null && connections.isEmpty()) {
+            val name = memberNames.remove(member) ?: member
+            broadcast("server", "Member left: $name.")
+        }
+    }
+
+    suspend fun who(sender: String) {
+        members[sender]?.send(Frame.Text(memberNames.values.joinToString(prefix = "[server::who] ")))
+    }
+
+    suspend fun help(sender: String) {
+        members[sender]?.send(Frame.Text("[server::help] Possible commands are: /user, /help and /who"))
+    }
+
+    suspend fun sendTo(recipient: String, sender: String, message: String) {
+        members[recipient]?.send(Frame.Text("[$sender] $message"))
+    }
+
+    suspend fun message(sender: String, message: String) {
+        val name = memberNames[sender] ?: sender
+        val formatted = "[$name] $message"
+
+        broadcast(formatted)
+
+        synchronized(lastMessages) {
+            lastMessages.add(formatted)
+            if (lastMessages.size > 100) {
+                lastMessages.removeFirst()
+            }
+        }
+    }
+
+    private suspend fun broadcast(message: String) {
+        members.values.forEach { socket ->
+            socket.send(Frame.Text(message))
+        }
+    }
+
+    private suspend fun broadcast(sender: String, message: String) {
+        val name = memberNames[sender] ?: sender
+        broadcast("[$name] $message")
+    }
+
+    suspend fun List<WebSocketSession>.send(frame: Frame) {
+        forEach {
+            try {
+                it.send(frame.copy())
+            } catch (t: Throwable) {
+                try {
+                    it.close(CloseReason(CloseReason.Codes.PROTOCOL_ERROR, ""))
+                } catch (ignore: ClosedSendChannelException) {
+                }
+            }
+        }
+    }
+}
