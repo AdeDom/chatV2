19c7e6ef4da2edfadef509b90fae3f732adc4634
AdeDom <pathiphonyw@gmail.com>
2020-09-26 22:22:15 +0700

    remove chat server

--- src/Application.kt
+++ src/Application.kt
@@ -9,17 +9,13 @@ import io.ktor.jackson.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
-import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.receiveAsFlow
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Duration
-import java.util.*
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CopyOnWriteArrayList
-import java.util.concurrent.atomic.AtomicInteger
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -46,6 +42,8 @@ fun Application.module() {
         pingPeriod = Duration.ofSeconds(60)
     }
 
+    val members = hashMapOf<String, MutableList<WebSocketSession>>()
+
     install(Routing) {
         route("api") {
             get("fetch-chat") {
@@ -58,93 +56,20 @@ fun Application.module() {
             }
         }
 
+        // TODO: 26/09/2563 add header by token
         route("webSocket") {
             webSocket("dru-chat") {
-                ChatServer.memberJoin("999", this)
-                try {
-                    incoming.receiveAsFlow().collect { frame ->
-                        ChatServer.message("999", (frame as Frame.Text).readText())
+                members.computeIfAbsent("-") { CopyOnWriteArrayList() }.add(this)
+
+                incoming.receiveAsFlow().collect { frame ->
+                    members.values.forEach { socket ->
+                        socket.forEach {
+                            it.outgoing.send(Frame.Text((frame as Frame.Text).readText()))
+                        }
                     }
-                } finally {
-                    ChatServer.memberLeft("999", this)
                 }
             }
         }
     }
 
-}
-
-object ChatServer {
-
-    private val usersCounter = AtomicInteger()
-
-    private val memberNames = ConcurrentHashMap<String, String>()
-
-    private val members = ConcurrentHashMap<String, MutableList<WebSocketSession>>()
-
-    private val lastMessages = LinkedList<String>()
-
-    suspend fun memberJoin(member: String, socket: WebSocketSession) {
-        val name = memberNames.computeIfAbsent(member) { "user${usersCounter.incrementAndGet()}" }
-
-        val list = members.computeIfAbsent(member) { CopyOnWriteArrayList() }
-        list.add(socket)
-
-        if (list.size == 1) {
-            broadcast("server", "Member joined: $name.")
-        }
-
-        val messages = synchronized(lastMessages) { lastMessages.toList() }
-        for (message in messages) {
-            socket.send(Frame.Text(message))
-        }
-    }
-
-    suspend fun memberLeft(member: String, socket: WebSocketSession) {
-        val connections = members[member]
-        connections?.remove(socket)
-
-        if (connections != null && connections.isEmpty()) {
-            val name = memberNames.remove(member) ?: member
-            broadcast("server", "Member left: $name.")
-        }
-    }
-
-    suspend fun message(sender: String, message: String) {
-        val name = memberNames[sender] ?: sender
-        val formatted = "[$name] $message"
-
-        broadcast(formatted)
-
-        synchronized(lastMessages) {
-            lastMessages.add(formatted)
-            if (lastMessages.size > 100) {
-                lastMessages.removeFirst()
-            }
-        }
-    }
-
-    private suspend fun broadcast(message: String) {
-        members.values.forEach { socket ->
-            socket.send(Frame.Text(message))
-        }
-    }
-
-    private suspend fun broadcast(sender: String, message: String) {
-        val name = memberNames[sender] ?: sender
-        broadcast("[$name] $message")
-    }
-
-    private suspend fun List<WebSocketSession>.send(frame: Frame) {
-        forEach {
-            try {
-                it.send(frame.copy())
-            } catch (t: Throwable) {
-                try {
-                    it.close(CloseReason(CloseReason.Codes.PROTOCOL_ERROR, ""))
-                } catch (ignore: ClosedSendChannelException) {
-                }
-            }
-        }
-    }
 }
